f82d2f4fb1ae1c734944d20dc940a53258aa9ab1
Zbyszek Zbyszek_macbook <zbyszekzbyszek_macbook@Zbyszeks-MacBook-Pro.local>
2023-12-27 07:40:26 +0100

    contact us tests

--- test/pageobjects/contact.page.ts
+++ test/pageobjects/contact.page.ts
@@ -1,16 +1,90 @@
-import { $ } from '@wdio/globals'
-import Page from './page';
+import { $ } from "@wdio/globals";
+import Page from "./page";
 
-/**
- * sub page containing specific selectors and methods for a specific page
- */
 class ContactPage extends Page {
-    /**
-     * define selectors using getter methods
-     */
-    public get flashAlert () {
-        return $('#flash');
-    }
+  private get inputName() {
+    return $("#name");
+  }
+
+  private get inputEmail() {
+    return $("#email");
+  }
+
+  private get surnameInput() {
+    return $("#surname");
+  }
+
+  private get sendMesageButton() {
+    return $("#submit_btn");
+  }
+
+  private get findUsOnMapTite() {
+    return $("h2=Find us on map");
+  }
+
+  private get findUsButton() {
+    return $("#find-us");
+  }
+
+  private get nameValidationError() {
+    return $("#Zebra_Form_error_message_name");
+  }
+
+  private get surnameValidationError() {
+    return $("#Zebra_Form_error_message_surname");
+  }
+
+  private get emailValidationError() {
+    return $("#Zebra_Form_error_message_email");
+  }
+
+  public clickFindUsButton() {
+    return this.findUsButton.click();
+  }
+
+  public async typeToNameInput(text: string) {
+    await this.typeToInput(await this.inputName, text);
+  }
+
+  public async typeToSurnameInput(text: string) {
+    await this.typeToInput(await this.surnameInput, text);
+  }
+
+  public async typeToEmailInput(text: string) {
+    await this.typeToInput(await this.inputEmail, text);
+  }
+
+  public clickSendMessageButton() {
+    return this.sendMesageButton.click();
+  }
+
+  public checkNameValidationErrorDisplayed() {
+    return expect(this.nameValidationError).toBeDisplayed();
+  }
+
+  public checkNameValidationErrorNotDisplayed() {
+    return expect(this.nameValidationError).not.toBeDisplayed();
+  }
+
+  public checkSurnameValidationErrorDisplayed() {
+    return expect(this.surnameValidationError).toBeDisplayed();
+  }
+
+  public checkSurnameValidationErrorNotDisplayed() {
+    return expect(this.surnameValidationError).not.toBeDisplayed();
+  }
+
+  public checkEmailValidationErrorDisplayed() {
+    return expect(this.emailValidationError).toBeDisplayed();
+  }
+
+  public checkEmailValidationErrorNotDisplayed() {
+    return expect(this.emailValidationError).not.toBeDisplayed();
+  }
+
+  public checkFindUsOnMapTitleDisplayed() {
+    return expect(this.findUsOnMapTite).toBeDisplayedInViewport();
+  }
 }
 
 export default new ContactPage();
---
+++ test/specs/contactUs.ts
@@ -0,0 +1,31 @@
+//import { expect } from "@wdio/globals";
+import HomePage from "../pageobjects/home.page";
+import ContactPage from "../pageobjects/contact.page";
+
+describe("Contact us", () => {
+  beforeEach(async () => {
+    await HomePage.openSiiPage();
+    await HomePage.clickContactUsButton();
+  });
+
+  it("check validation errors displayed in contact form", async () => {
+    await ContactPage.clickSendMessageButton();
+    await ContactPage.checkNameValidationErrorDisplayed();
+    await ContactPage.checkSurnameValidationErrorDisplayed();
+    await ContactPage.checkEmailValidationErrorDisplayed();
+
+    await ContactPage.typeToNameInput("test");
+    await ContactPage.typeToSurnameInput("test");
+    await ContactPage.typeToEmailInput("test@test.pl");
+
+    await ContactPage.clickSendMessageButton();
+    await ContactPage.checkEmailValidationErrorNotDisplayed();
+    await ContactPage.checkNameValidationErrorNotDisplayed();
+    await ContactPage.checkSurnameValidationErrorNotDisplayed();
+  });
+
+  it("should check find us button", async () => {
+    await ContactPage.clickFindUsButton();
+    await ContactPage.checkFindUsOnMapTitleDisplayed();
+  });
+});
